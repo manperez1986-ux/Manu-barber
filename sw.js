808fcb24a45c40ab2a98ab652adefbb37d021a99
manperez1986-ux <manperez1986@gmail.com>
2026-03-01 01:02:32 -0400

    sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'manu-barber-v53';
+const assets = [
+  './',
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js',
+  'https://unpkg.com/lucide@latest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
